5ddf34ff7966aa4cd40168aab395094352d00b33
jcigama <jcigama77@yahoo.com>
2021-03-04 21:26:39 -0500

    added sticky functionality for gender interest

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -28,7 +28,6 @@ class Controller
     function personal()
     {
         global $validator;
-        global $profile;
 
         //If user submits data
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -39,45 +38,49 @@ class Controller
             $age = trim($_POST['age']);
             $gender = $_POST['genders'];
             $number = trim($_POST['number']);
+            $premium = $_POST['premium'];
 
             //Validation
-            if ($validator->validfName($fName)) {
-                $profile->setfName($fName);
-                $_SESSION['fName'] = $fName;
-            } else {
+            if (!$validator->validfName($fName)) {
                 $this->_f3->set('errors["fName"]', "Invalid first name. Must contain only alphabetical characters and can't be empty.");
             }
 
-            if ($validator->validlName($lName)) {
-                $profile->setlName($lName);
-                $_SESSION['lName'] = $lName;
-            } else {
+            if (!$validator->validlName($lName)) {
                 $this->_f3->set('errors["lName"]', "Invalid last name. Must contain only alphabetical characters and can't be empty.");
             }
 
-            if ($validator->validAge($age)) {
-                $profile->setAge($age);
-                $_SESSION['age'] = $age;
-            } else {
+            if (!$validator->validAge($age)) {
                 $this->_f3->set('errors["age"]', "Invalid age. Must be between 18 - 118.");
             }
 
-            if ($validator->validPhone($number)) {
-                $profile->setPhone($number);
-                $_SESSION['number'] = $number;
-            } else {
+            if (!$validator->validPhone($number)) {
                 $this->_f3->set('errors["number"]', "Invalid phone number. Must be 10-11 digits");
             }
 
-            if (isset($gender)) {
-                $profile->setGender($gender);
-                $_SESSION['gender'] = $gender;
-            } else {
+            if (!isset($gender)) {
                 $this->_f3->set('errors["genders"]', "Must choose a gender");
             }
 
-            //If there are no errors, redirect user to profile
+            //If there are no errors,
+            // instantiate appropriate member object and redirect user to profile.
             if (empty($this->_f3->get('errors'))) {
+
+                //If user signs up for premium
+                if($premium == "on") {
+                    $member = new PremiumMember($fName, $lName, $age, $gender, $number);
+
+                    $_SESSION['$member'] =
+                        serialize($member);
+
+                //If does not user signs up for premium
+                } else {
+
+                    $member = new Member($fName, $lName, $age, $gender, $number);
+
+                    $_SESSION['$member'] =
+                        serialize($member);
+                }
+
                 $this->_f3->reroute('/profileInfo');
             }
         }
@@ -88,6 +91,7 @@ class Controller
         $this->_f3->set('age', isset($age) ? $age : "");
         $this->_f3->set('gender', isset($gender) ? $gender : "");
         $this->_f3->set('number', isset($number) ? $number : "");
+        $this->_f3->set('premium', isset($premium) ? $premium : "");
 
         //Render view
         $view = new Template();
@@ -97,45 +101,44 @@ class Controller
     /** Display profile info page */
     function profile()
     {
-        global $validator;
-        global $profile;
+        //unserialize session and reassign to an instance variable
+        $member = unserialize($_SESSION['$member']);
 
         //If user submits data
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            var_dump($_POST);
             $email = trim($_POST['email']);
             $state = $_POST['state'];
             $genderInterest = $_POST['genderInterest'];
             $biography = $_POST['biography'];
 
             //Validation
-            if ($validator->validEmail($email)) {
-                $profile->setEmail($email);
-                $_SESSION['email'] = $email;
-
-            } else {
+            if (!$validator->validEmail($email)) {
                 $this->_f3->set('errors["email"]', "Invalid email. Please enter valid email.");
             }
 
-            //Optional fields
-            $profile->setGenderInterest($genderInterest);
-            $_SESSION['genderInterest'] = $genderInterest;
-
-            $profile->setBiography($biography);
-            $_SESSION['biography'] = $biography;
-
-            $profile->setState($state);
-            $_SESSION['state'] = $state;
-
-            //If there are no errors, redirect user to interests
             if (empty($this->_f3->get('errors'))) {
-                $this->_f3->reroute('/interests');
+
+                //Call set methods to assign parameters
+                $member->setEmail($email);
+                $member->setState($state);
+                $member->setSeeking($genderInterest);
+                $member->setBio($biography);
+
+                $_SESSION['$member'] = serialize($member);
+
+//                if ($member->isMember()) {
+//                    $this->_f3->reroute('/interests');
+//                } else {
+//                    $this->_f3->reroute('/summary');
+//                }
             }
         }
 
         //Sticky data
         $this->_f3->set('email', isset($email) ? $email : "");
         $this->_f3->set('state', isset($state) ? $state : "");
-        $this->_f3->set('genderInterest', isset($genderInterest) ?  : "");
+        $this->_f3->set('genderInterest', isset($genderInterest) ? $genderInterest : "");
         $this->_f3->set('biography', isset($biography) ? $biography : "");
 
         //Display a view
